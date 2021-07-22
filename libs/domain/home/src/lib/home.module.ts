9f9c9277b86ca135547befacecf2d199cbf0d12d
adglow-izemfr <izemfr@adglow.com>
2021-07-22 21:07:12 +0100

    feat: generate home page

---
+++ libs/domain/home/src/lib/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HomePage } from './home.page';
+import { CategoryListComponent } from './category-list/category-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+    ]),
+  ],
+  declarations: [
+    HomePage,
+    CategoryListComponent
+  ],
+})
+export class HomeModule {}
