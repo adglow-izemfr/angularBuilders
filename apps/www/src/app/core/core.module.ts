a95027d799af46b6189055814ceb0300bd943e42
adglow-izemfr <izemfr@adglow.com>
2021-07-25 10:27:27 +0100

    2.3 CodeLab router events

--- apps/www/src/app/core/core.module.ts
+++ apps/www/src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 import { CoreRoutingModule } from './core-routing.module';
 
 @NgModule({
@@ -11,4 +13,18 @@ import { CoreRoutingModule } from './core-routing.module';
     HttpClientModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(router: Router) {
+    router.events.pipe(
+      filter(routerEvent => routerEvent instanceof NavigationEnd),
+      map(routerEvent => routerEvent as NavigationEnd),
+      ).subscribe({
+        next: (navigation: NavigationEnd) =>
+          console.log({
+            category: 'BUSINESS',
+            event: 'NAV',
+            label: navigation.urlAfterRedirects,
+          }),
+      });
+  }
+}
