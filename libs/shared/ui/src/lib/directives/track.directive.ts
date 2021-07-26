c327c6163cc6cb3419dd0a795f1bda6e1958e715
adglow-izemfr <izemfr@adglow.com>
2021-07-26 15:20:34 +0200

    5.3 CodeLabs directive

--- libs/shared/ui/src/lib/directives/track.directive.ts
+++ libs/shared/ui/src/lib/directives/track.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[abUiTrack]'
 })
 export class TrackDirective {
 
-  constructor() { }
+  @Input('abUiTrack') label?: string;
+
+  @HostListener('click') onClick() {
+    console.log((this.label || 'Hello directives ') + this.el.nativeElement.innerHTML);
+  }
+
+  constructor(private el: ElementRef) {
+
+  }
 
 }
